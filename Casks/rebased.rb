a699b04bcdaaa77b451433f89313a18c5ab4ac78
detachhead <detachhead@users.noreply.github.com>
2026-05-05 19:57:18 +1000

    try to fix ci

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -1,11 +1,26 @@
 cask "rebased" do
-  arch arm: "-aarch64", intel: ""
-
   version "1.0.12"
-  sha256 arm:   "3ef9aace106128e78e94777c7fe64228cfa1df816e7cc15b8b1bc054b7df9e9c",
-         intel: "93bc02e6c7ba06e907cfa540ed22d9eae0a7e3408810bf3bf07cd18a8bef6cdc"
 
-  url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased#{arch}.dmg"
+  on_macos do
+    on_arm do
+      sha256 "3ef9aace106128e78e94777c7fe64228cfa1df816e7cc15b8b1bc054b7df9e9c"
+
+      url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased-aarch64.dmg"
+    end
+    on_intel do
+      sha256 "93bc02e6c7ba06e907cfa540ed22d9eae0a7e3408810bf3bf07cd18a8bef6cdc"
+
+      url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased.dmg"
+    end
+
+    app "Rebased.app"
+
+    postflight do
+      system_command "xattr",
+                     args: ["-rd", "com.apple.quarantine", "#{appdir}/Rebased.app"]
+    end
+  end
+
   name "Rebased"
   desc "Git client based on the IntelliJ platform"
   homepage "https://github.com/DetachHead/rebased"
@@ -13,13 +28,6 @@ cask "rebased" do
   livecheck do
     url :url
     strategy :github_latest
-  end
-
-  app "Rebased.app"
-
-  postflight do
-    system_command "xattr",
-                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Rebased.app"]
   end
 
   uninstall quit: "io.github.detachhead.rebased"
