f2a2b82d259a2293fb3aa0543e73accad6b73760
Francesco Lucarelli <67560467+fraluc06@users.noreply.github.com>
2026-05-06 14:58:03 +0200

    Refactor rebased.rb for architecture support

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -2,23 +2,12 @@ cask "rebased" do
   version "1.0.12"
 
   on_macos do
-    on_arm do
-      sha256 "3ef9aace106128e78e94777c7fe64228cfa1df816e7cc15b8b1bc054b7df9e9c"
+    arch arm: "-aarch64", intel: ""
 
-      url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased-aarch64.dmg"
-    end
-    on_intel do
-      sha256 "93bc02e6c7ba06e907cfa540ed22d9eae0a7e3408810bf3bf07cd18a8bef6cdc"
+    sha256 arm:   "3ef9aace106128e78e94777c7fe64228cfa1df816e7cc15b8b1bc054b7df9e9c",
+           intel: "93bc02e6c7ba06e907cfa540ed22d9eae0a7e3408810bf3bf07cd18a8bef6cdc"
 
-      url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased.dmg"
-    end
-
-    app "Rebased.app"
-
-    postflight do
-      system_command "xattr",
-                     args: ["-rd", "com.apple.quarantine", "#{appdir}/Rebased.app"]
-    end
+    url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased#{arch}.dmg"
   end
 
   name "Rebased"
@@ -28,6 +17,13 @@ cask "rebased" do
   livecheck do
     url :url
     strategy :github_latest
+  end
+
+  app "Rebased.app"
+
+  postflight do
+    system_command "xattr",
+                   args: ["-rd", "com.apple.quarantine", "#{appdir}/Rebased.app"]
   end
 
   uninstall quit: "io.github.detachhead.rebased"
