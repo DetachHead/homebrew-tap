6bf5d11d49f32bef8a651f5a4b7d4624c086139b
DetachHead <57028336+DetachHead@users.noreply.github.com>
2026-05-27 13:55:11 +0000

    rebased 1.1.1

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -1,11 +1,11 @@
 cask "rebased" do
-  version "1.1.0"
+  version "1.1.1"
 
   on_macos do
     arch arm: "-aarch64", intel: ""
 
-    sha256 arm:   "23991cf5fc21c376355ad22b842f6c0f6ddae1d0598f6ed7a88493067994e190",
-           intel: "15fbfe8e7a4e951064834c82256c3ce33e6fc26753e7a1116daa0db0bb0033cc"
+    sha256 arm:   "fa9b333ae62d46347b295e6da0f8478e27fa24dd82ae08385c79303771ef366e",
+           intel: "04fe5ff20693343099096e933ffaa248b1d52abbac9794ec39b20306d6681caa"
 
     url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased#{arch}.dmg"
   end
