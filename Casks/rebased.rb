5fc49c51000ef5e016e394bf91e5071a0ad423c1
DetachHead <57028336+DetachHead@users.noreply.github.com>
2026-06-07 12:32:44 +0000

    rebased 1.1.3
    Closes #22.
    
    Signed-off-by: DetachHead <57028336+DetachHead@users.noreply.github.com>

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -1,11 +1,11 @@
 cask "rebased" do
-  version "1.1.2"
+  version "1.1.3"
 
   on_macos do
     arch arm: "-aarch64", intel: ""
 
-    sha256 arm:   "853e8f22672e89616f81f624710a2aefbc505d6112b6f58e642fea44f4cabb10",
-           intel: "e0ec0b631ee44815dd2a99b8cf48dbf39c88754f384fc65522a8f5180eea0968"
+    sha256 arm:   "58b14892b8d4af4d787336735ac62acf6a76dce535bfb14b3c36bd3bfe8ec96b",
+           intel: "8e5d8da51b41c39e7c82220e29ce983fbd3b69578e6b51dcbff18e648e66afb5"
 
     url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased#{arch}.dmg"
   end
