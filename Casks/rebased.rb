f11e3761d762a0092900f58a265e0985fffb93e2
Anka <runner@sjc22-bt150-624d5d47-facd-4a26-99d6-0d1d0d7b9e61-02EA2940A23C.local>
2026-06-28 05:22:35 +0000

    rebased 1.1.4

--- Casks/rebased.rb
+++ Casks/rebased.rb
@@ -1,11 +1,11 @@
 cask "rebased" do
-  version "1.1.3"
+  version "1.1.4"
 
   on_macos do
     arch arm: "-aarch64", intel: ""
 
-    sha256 arm:   "58b14892b8d4af4d787336735ac62acf6a76dce535bfb14b3c36bd3bfe8ec96b",
-           intel: "8e5d8da51b41c39e7c82220e29ce983fbd3b69578e6b51dcbff18e648e66afb5"
+    sha256 arm:   "3803da85a9302926f8c09d644e2bfc7799f4fe39a2dfba8c613aa51c9eebf7dd",
+           intel: "f9d0d5b04902f561691fbf2dd1c84b31b6e9c6bbff5fd44d07714067834bc649"
 
     url "https://github.com/DetachHead/rebased/releases/download/#{version}/rebased#{arch}.dmg"
   end
